10e438bea51443120662e7f875e00b33ffb94cbd
Tim <acronym1986@gmail.com>
2021-03-18 01:03:31 +1100

    complated declaration

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file!
+
+
+//declare variable companyName
+const companyName = "Scuber";
+
+
+//declare varibale by using let
+let mostProfitableNeighborhood = "Chelsea";
+
+
+//declare variable by using let 2
+let companyCeo = "Susan Smith";
